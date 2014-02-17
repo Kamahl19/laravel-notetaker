b0476418dae50f84df1632a8342a08c722891d65
Martin Litvaj <kamahl19@gmail.com>
2014-02-17 14:44:02 +0100

    changed database name

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'msproject',
+			'database'  => 'laravel_planner',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',

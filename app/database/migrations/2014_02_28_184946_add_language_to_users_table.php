83ce5621902ecb0963d86ac18c4db9d4eacd7726
Martin Litvaj <kamahl19@gmail.com>
2014-03-01 12:07:33 +0100

    Added timezone and language to user table

---
+++ app/database/migrations/2014_02_28_184946_add_language_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddLanguageToUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+      $table->string('language');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->dropColumn('language');
+		});
+	}
+
+}
